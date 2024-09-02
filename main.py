cd8e5c089cf86454c4df7af6d5c5a0eca77e8a81
curiousKidd <skasrjhaan@gmail.com>
2024-09-02 23:51:07 +0900

    pipenv to venv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import openai, requests
+import openai
+import requests
 
 # chat gpt를 활용하면... 개발할 수없을까?
 # # 날씨 API 설정
@@ -15,4 +16,7 @@ weather_data = weather_response.json()
 
 # GPT에 질문 생성 및 응답 받기
 question = f"Seoul의 현재 날씨는 다음과 같습니다: {weather_data}. 이에 대한 요약을 해주세요."
-# response = openai.Completion.create(engine="text-davinci-004", prompt=question, max_tokens=100)
+response = openai.Completion.create(engine="text-davinci-004", prompt=question, max_tokens=100)
+
+# 응답 출력 또는 카카오톡 메시지로 보내기
+print(response['choices'][0]['text'])

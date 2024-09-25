2f1183418f1d6b4ffed109031a55a4ca1af9f81c
curiousKidd <skasrjhaan@gmail.com>
2024-09-25 23:45:16 +0900

    run test

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-import openai
+from openai import OpenAI
 import requests
+import os
 
+openai = OpenAI(api_key="")
 
 def get_tomorrow_weather():
     # chat gpt를 활용하면... 개발할 수없을까?
 
     # 날씨 API 설정
-    weather_api_key = "43604b3735cff561b8407ea5f50d6acc"
+    os.getenv('OPENAI_API_KEY')
+    weather_api_key = ""
     location = "Seoul"
     weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={weather_api_key}"
 
-    print("111111111111111111")
-    print("111111111111111111")
+    weather_response = requests.get(weather_url)
 
     # GPT API 설정
-    openai.api_key = "YOUR_OPENAI_API_KEY"
 
     # 날씨 정보 가져오기
-    weather_response = requests.get(weather_url)
     weather_data = weather_response.json()
 
+    print(weather_data)
+
     # GPT에 질문 생성 및 응답 받기
-    question = f"Seoul의 현재 날씨는 다음과 같습니다: {weather_data}. 이에 대한 요약을 해주세요."
-    response = openai.Completion.create(engine="text-davinci-004", prompt=question, max_tokens=100)
+    text = f"Seoul의 현재 날씨는 다음과 같습니다: {weather_data}. 이에 대한 요약을 해주세요."
+    response = openai.completions.create(
+        # model="gpt-4o-mini",
+        model="gpt-3.5-turbo-instruct",
+        prompt=text,
+        max_tokens=300
+    )
 
     # 응답 출력 또는 카카오톡 메시지로 보내기
-    return response['choices'][0]['text']
+    return response.choices[0].text
+
+
+
+get_tomorrow_weather()

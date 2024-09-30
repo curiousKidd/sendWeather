aabda767d899711a7aa548d7eb9ec55270417831
curiousKidd <skasrjhaan@gmail.com>
2024-09-30 22:26:23 +0900

    test open ai

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from openai import OpenAI
+
+
 import requests
 import os
 
-openai = OpenAI(api_key="")
+client = OpenAI(api_key=os.getenv("OPEN_AI_KEY")) 
+
 
 def get_tomorrow_weather():
     # chat gpt를 활용하면... 개발할 수없을까?
 
+    print(os.getenv("OPEN_AI_KEY"))
+
     # 날씨 API 설정
-    os.getenv('OPENAI_API_KEY')
     weather_api_key = ""
     location = "Seoul"
     weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={weather_api_key}"
@@ -24,12 +28,10 @@ def get_tomorrow_weather():
 
     # GPT에 질문 생성 및 응답 받기
     text = f"Seoul의 현재 날씨는 다음과 같습니다: {weather_data}. 이에 대한 요약을 해주세요."
-    response = openai.completions.create(
-        # model="gpt-4o-mini",
-        model="gpt-3.5-turbo-instruct",
-        prompt=text,
-        max_tokens=300
-    )
+    response = client.completions.create(# model="gpt-4o-mini",
+    model="gpt-3.5-turbo-instruct",
+    prompt=text,
+    max_tokens=300)
 
     # 응답 출력 또는 카카오톡 메시지로 보내기
     return response.choices[0].text
---
+++ test.py
@@ -0,0 +1,20 @@
+import openai
+import os
+
+
+
+print(os.getenv("OPEN_AI_KEY"))
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
